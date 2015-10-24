3d1715e598394a5aad38070efb6ab5d8aa4e1696
Joaquin Perez <jperezr21@gmail.com>
2015-10-23 23:06:47 -0300

    Basic models and admin interface

--- WebApp/models.py
+++ WebApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Cancha(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=200)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Telefono(models.Model):
+    numero = models.CharField(max_length=15)
+    cancha = models.ForeignKey(Cancha, related_name="telefonos", related_query_name="telefono")
+
+    def __unicode__(self):
+        return self.numero
