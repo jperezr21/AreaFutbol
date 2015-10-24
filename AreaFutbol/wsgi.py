b41bfd4cb34f3f27c4ebead9d4fd8c0379a315dc
Joaquin Perez <jperezr21@gmail.com>
2015-10-23 23:51:23 -0300

    Config de apache

--- AreaFutbol/wsgi.py
+++ AreaFutbol/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/AreaFutbol')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/AreaFutbol/egg_cache")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AreaFutbol.settings")
 
 from django.core.wsgi import get_wsgi_application
